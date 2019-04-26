a152ac650b0bb2efea5e4ea342e7ec59142e7e3f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-04-26 17:25:20 +0900

    Just forgot to add 'camera_matrix.*'

---
+++ src/util/camera_matrix.cpp
@@ -0,0 +1,24 @@
+#include "util/camera_matrix.h"
+#include "util/NumType.h"
+
+
+namespace dso {
+
+Mat33f initializeCameraMatrix(const float fx, const float fy,
+                              const float cx, const float cy) {
+    Mat33f K = Mat33f::Zero();
+    K(0,0) = fx;
+    K(1,1) = fy;
+    K(0,2) = cx;
+    K(1,2) = cy;
+    K(2,2) = 1;
+    return K;
+}
+
+
+Mat33f createCameraMatrixFromCalibHessian(CalibHessian &HCalib) {
+    return initializeCameraMatrix(HCalib.fxl(), HCalib.fyl(),
+                                  HCalib.cxl(), HCalib.cyl());
+}
+
+}
---
+++ src/util/camera_matrix.h
@@ -0,0 +1,8 @@
+#include "util/NumType.h"
+#include "FullSystem/HessianBlocks.h"
+
+namespace dso {
+Mat33f initializeCameraMatrix(const float fx, const float fy,
+                              const float cx, const float cy);
+Mat33f createCameraMatrixFromCalibHessian(CalibHessian &HCalib);
+}
